6804fff1a29c422b5022639cfea3026b7a738b95
Lapalu Liyanage <lapaluliyanage@gmail.com>
2025-04-12 22:49:05 +0530

    update_test_loop

--- JSCourseTesting/scope_lab.js
+++ JSCourseTesting/scope_lab.js
@@ -15,3 +15,35 @@ const blockConst = "I'm a block-scoped const";
 console.log(globalVar); // Output: "I'm a global variable"
 console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
+
+let userRole = "admin";
+let accessLevel ;
+
+if(userRole === "admin"){
+    accessLevel = "full";
+}
+else if(userRole === "editor"){
+    accessLevel = "partial";
+}
+else{
+    accessLevel = "none";
+}
+
+console.log(accessLevel); // Output: "full"
+
+let isLoggedIn = true;
+let userMessage;
+
+if(isLoggedIn){
+    if(userRole === "admin"){
+        userMessage = "Welcome Admin! You have full access.";
+    }
+    else if(userRole === "editor"){
+        userMessage = "Welcome Editor! You have partial access.";
+    }
+    else{
+        userMessage = "please log in to access your account.";
+    }
+}
+
+console.log("User Message:", userMessage);
